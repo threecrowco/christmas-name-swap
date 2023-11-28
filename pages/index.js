a40874c5de19ada2f37d4fd89707a6cc4cb437a7
Chris Canterbury <threecrowco@tcc-server.local>
2023-11-28 14:05:56 -0600

    init

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Layout from "@/components/layout";
+
+export default function HomePage() {
+    return (
+        <Layout>
+            <div>
+                <h1>Home Page</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quod eos, unde laboriosam ea et esse ut hic voluptatibus
+                    sequi porro vero, iusto assumenda illo placeat.
+                </p>
+            </div>
+        </Layout>
+    );
+}
